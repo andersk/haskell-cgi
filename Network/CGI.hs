e26943c74321eb2e317a684e23c37e8e8a582b0e
Björn Bringert <bjorn@bringert.net>
2005-06-14 17:40:45 +0000

    Copied files from BloB repo.

---
+++ Network/CGI.hs
@@ -0,0 +1,268 @@
+-----------------------------------------------------------------------------
+-- |
+-- Module      :  Network.CGI
+-- Copyright   :  (c) The University of Glasgow 2001
+--                (c) Bjorn Bringert 2004
+-- License     :  BSD-style (see the file libraries/network/LICENSE)
+-- 
+-- Maintainer  :  bjorn@bringert.net
+-- Stability   :  experimental
+-- Portability :  non-portable (uses Network.URI)
+--
+-- Simple library for writing CGI programs.
+--
+-- Based on the original Haskell binding for CGI:
+--
+-- Original Version by Erik Meijer <mailto:erik@cs.ruu.nl>
+-- Further hacked on by Sven Panne <mailto:sven.panne@aedion.de>
+-- Further hacking by Andy Gill <mailto:andy@galconn.com>
+--
+-----------------------------------------------------------------------------
+
+module Network.CGI (
+			  CGI, CGIResult
+			 , output, redirect
+			 , io, getVar, 
+			 , getInput, readInput, getInputs
+			 , runCGI
+			 , setHeader
+			 , Cookie(..), newCookie
+			 , getCookie, setCookie, deleteCookie
+			 ) where
+
+import Control.Monad.State
+import Data.Maybe (listToMaybe)
+import Network.HTTP.Cookie (Cookie(..), newCookie, findCookie)
+import qualified Network.HTTP.Cookie as Cookie (setCookie, deleteCookie)
+import Network.URI (unEscapeString)
+import System.Environment ( getEnv )
+
+data CGIState = CGIState {
+			  cgiVars :: [(String,String)],
+			  cgiInput :: [(String,String)],
+			  cgiResponseHeaders :: [(String,String)]
+			 }
+	      deriving (Show, Read, Eq, Ord)
+
+type CGI a = StateT CGIState IO a 
+
+data CGIResult = CGIOutput String
+	       | CGIRedirect String
+		 deriving (Show, Read, Eq, Ord)
+
+--
+-- * CGI monad
+--
+
+-- | Perform an IO action in the CGI monad
+io :: IO a -> CGI a
+io = lift
+
+-- | note: if using Windows, you might need to wrap 'withSocketsDo' round main.
+runCGI :: CGI CGIResult -> IO ()
+runCGI f = do qs <- getQueryString
+	      vars <- getCgiVars
+	      let s = CGIState{
+			       cgiVars = vars,
+			       cgiInput = formDecode qs,
+			       cgiResponseHeaders = initHeaders
+			      }
+  	      (output,s') <- runStateT f s
+	      let hs = cgiResponseHeaders s'
+	      case output of
+			  CGIOutput str   -> doOutput str hs
+			  CGIRedirect url -> doRedirect url hs
+
+
+doOutput :: String -> [(String,String)] -> IO ()
+doOutput str hs = 
+    do
+    let hs' = tableAddIfNotPresent "Content-type" "text/html; charset=ISO-8859-1" hs
+    printHeaders hs'
+    putStrLn ""
+    putStr str
+
+doRedirect :: String -> [(String,String)] -> IO ()
+doRedirect url hs =
+    do
+    let hs' = tableSet "Location" url hs
+    printHeaders hs'
+    putStrLn ""
+
+--
+-- * Output / redirect
+--
+
+output :: String -> CGI CGIResult
+output str = return $ CGIOutput str
+
+redirect :: String -> CGI CGIResult
+redirect str = return $ CGIRedirect str
+
+--
+-- * HTTP variables
+--
+
+-- | Get a CGI variable. Example:
+--
+-- > remoteAddr <- getVar "REMOTE_ADDR"
+getVar :: String -> CGI (Maybe String)
+getVar name = gets (lookup name . cgiVars)
+
+--
+-- * Query input
+--
+
+-- | Get an input variable, for example from a form.
+--   Example:
+--
+-- > query <- getInput "query"
+getInput :: String -> CGI (Maybe String)
+getInput name = gets (lookup name . cgiInput)
+
+-- | Same as 'getInput', but tries to read the value to the desired type.
+readInput :: Read a => 
+	     String 
+	  -> CGI (Maybe a) -- ^ 'Nothing' if the variable does not exist
+	                   --   or if the value could not be interpreted
+	                   --   as the desired type.
+readInput name = liftM (>>= fmap fst . listToMaybe . reads) (getInput name)
+
+-- | Get all input variables and their values.
+getInputs :: CGI [(String,String)]
+getInputs = gets cgiInput
+
+--
+-- * Cookies
+--
+
+-- | Get the value of a cookie
+getCookie :: String -> CGI (Maybe String)
+getCookie name = do
+		 cs <- getVar "HTTP_COOKIE"
+		 return $ maybe Nothing (findCookie name) cs
+
+-- | Set a cookie
+setCookie :: Cookie -> CGI ()
+setCookie cookie = 
+    modify (\s -> s{cgiResponseHeaders 
+		    = Cookie.setCookie cookie (cgiResponseHeaders s)})
+
+-- | Delete a cookie from the client
+deleteCookie :: Cookie -> CGI ()
+deleteCookie cookie = setCookie (Cookie.deleteCookie cookie)
+
+
+--
+-- * Headers
+--
+
+-- | Set a response header
+setHeader :: String -> String -> CGI ()
+setHeader name value = 
+    modify (\s -> s{cgiResponseHeaders 
+		    = tableSet name value (cgiResponseHeaders s)})
+
+
+showHeader :: (String,String) -> String
+showHeader (n,v) = n ++ ": " ++ v
+
+printHeaders :: [(String,String)] -> IO ()
+printHeaders = mapM_ (putStrLn . showHeader)
+
+initHeaders :: [(String,String)]
+initHeaders = []
+
+--
+-- * Utilities
+--
+
+
+-- | Get the name-value pairs from application/x-www-form-urlencoded data
+formDecode :: String -> [(String,String)]
+formDecode "" = []
+formDecode s = (urlDecode n, urlDecode (drop 1 v)) : formDecode (drop 1 rs)
+    where (nv,rs) = break (=='&') s
+	  (n,v) = break (=='=') nv
+
+
+-- | Convert a single value from the application/x-www-form-urlencoded encoding.
+urlDecode :: String -> String
+urlDecode = unEscapeString . replace '+' ' '
+
+
+-- | Replace all instances of a value in a list by another value.
+replace :: Eq a => 
+	   a   -- ^ Value to look for
+	-> a   -- ^ Value to replace it with
+	-> [a] -- ^ Input list
+	-> [a] -- ^ Output list
+replace x y = map (\z -> if z == x then y else z)
+
+
+tableSet :: Eq a => a -> b -> [(a,b)] -> [(a,b)]
+tableSet k v [] = [(k,v)]
+tableSet k v ((k',v'):ts) 
+    | k == k' = (k,v) : ts
+    | otherwise = (k',v') : tableSet k v ts
+
+tableAddIfNotPresent :: Eq a => a -> b -> [(a,b)] -> [(a,b)]
+tableAddIfNotPresent k v [] = [(k,v)]
+tableAddIfNotPresent k v ((k',v'):ts) 
+    | k == k' = (k',v') : ts
+    | otherwise = (k',v') : tableAddIfNotPresent k v ts
+
+--
+-- * CGI protocol stuff
+--
+
+getCgiVars :: IO [(String,String)]
+getCgiVars = do vals <- mapM myGetEnv cgiVarNames
+                return (zip cgiVarNames vals)
+
+cgiVarNames :: [String]
+cgiVarNames =
+   [ "DOCUMENT_ROOT"
+   , "AUTH_TYPE"
+   , "GATEWAY_INTERFACE"
+   , "SERVER_SOFTWARE"
+   , "SERVER_NAME"
+   , "REQUEST_METHOD"
+   , "SERVER_ADMIN"
+   , "SERVER_PORT"
+   , "QUERY_STRING"
+   , "CONTENT_LENGTH"
+   , "CONTENT_TYPE"
+   , "REMOTE_USER"
+   , "REMOTE_IDENT"
+   , "REMOTE_ADDR"
+   , "REMOTE_HOST"
+   , "TZ"
+   , "PATH"
+   , "PATH_INFO"
+   , "PATH_TRANSLATED"
+   , "SCRIPT_NAME"
+   , "SCRIPT_FILENAME"
+   , "HTTP_COOKIE"
+   , "HTTP_CONNECTION"
+   , "HTTP_ACCEPT_LANGUAGE"
+   , "HTTP_ACCEPT"
+   , "HTTP_HOST"
+   , "HTTP_UA_COLOR"
+   , "HTTP_UA_CPU"
+   , "HTTP_UA_OS"
+   , "HTTP_UA_PIXELS"
+   , "HTTP_USER_AGENT"
+   ]                      
+
+getQueryString :: IO String
+getQueryString = do
+   method <- myGetEnv "REQUEST_METHOD"
+   case method of
+      "POST" -> do len <- myGetEnv "CONTENT_LENGTH"
+                   inp <- getContents
+                   return (take (read len) inp)
+      _      -> myGetEnv "QUERY_STRING"
+
+myGetEnv :: String -> IO String
+myGetEnv v = Prelude.catch (getEnv v) (const (return ""))
---
+++ Network/HTTP/Cookie.hs
@@ -0,0 +1,199 @@
+-----------------------------------------------------------------------------
+-- |
+-- Module      :  Network.HTTP.Cookie
+-- Copyright   :  (c) Bjorn Bringert, 2004          
+-- License     :  BSD-style (see the file libraries/base/LICENSE)
+-- 
+-- Maintainer  :  Bjorn Bringert <bjorn@bringert.net>
+-- Stability   :  experimental
+-- Portability :  non-portable (uses Network.URI)
+--
+--  General server side HTTP cookie library.
+--  Based on <http://wp.netscape.com/newsref/std/cookie_spec.html>
+--
+-- TODO
+--
+-- Add client side stuff (basically parsing Set-Cookie: value)
+--
+-----------------------------------------------------------------------------
+module Network.HTTP.Cookie (
+			    Headers, Cookie(..)
+			    , newCookie
+			    , getCookie, setCookie, 
+			    , findCookie, deleteCookie
+			    , showCookie, readCookies
+			   ) where
+
+import Data.Char (toLower,isSpace)
+import Data.List (intersperse,find)
+import Data.Maybe (catMaybes)
+import Network.URI (escapeString, unEscapeString, reserved)
+import System.Locale (defaultTimeLocale, rfc822DateFormat)
+import System.Time (CalendarTime(..), Month(..), Day(..),
+		    formatCalendarTime)
+import Text.ParserCombinators.Parsec
+
+--
+-- * Types
+--
+
+type Headers = [(String,String)]
+
+-- | Contains all information about a cookie set by the server.
+data Cookie = Cookie {
+		      -- | Name of the cookie
+		      cookieName :: String,
+		      -- | Value of the cookie
+		      cookieValue :: String,
+		      -- | Expiry date of the cookie. If 'Nothing', the
+		      --   cookie expires when the browser sessions ends.
+		      --   If the date is in the psat, the client should
+		      --   delete cookie the cookie immediately.
+		      cookieExpires :: Maybe CalendarTime,
+		      -- | The domain suffix to which this cookie will be sent
+		      cookieDomain :: Maybe String,
+		      -- | The path to which this cookie will be sent
+		      cookiePath :: Maybe String,
+		      -- | 'True' is this cookie should only be sent using
+		      --   secure means.
+		      cookieSecure :: Bool
+		     }
+	    deriving (Show, Read, Eq, Ord)
+
+--
+-- * Constructing cookies
+--
+
+-- | Construct a cookie with only name and value set.
+--   This client will expire when the browser sessions ends,
+--   will only be sent to the server and path which set it
+--   and may be sent using any means.
+newCookie :: String -- ^ Name 
+	  -> String -- ^ Value
+	  -> Cookie -- ^ Cookie
+newCookie name value = Cookie { cookieName = name, 
+				cookieValue = value,
+				cookieExpires = Nothing,
+				cookieDomain = Nothing, 
+				cookiePath = Nothing,
+				cookieSecure = False 
+			      }
+
+--
+-- * Getting and setting cookies
+--
+
+-- | Get the value of a cookie from HTTP request headers
+getCookie :: Headers 
+	  -> String       -- ^ Cookie name
+	  -> Maybe String -- ^ Cookie value
+getCookie hs name = 
+    find (equalsIgnoreCase "Cookie" . fst) hs >>= findCookie name . snd
+
+-- | Get the value of a cookie from a semicolon separated list of 
+--   name-value pairs such as that in the value of the Cookie: header
+--   or the HTTP_COOKIE CGI variable.
+findCookie :: String -- ^ Cookie name
+	   -> String -- ^ Semicolon separated list of name-value pairs 
+	   -> Maybe String  -- ^ Cookie value, if found
+findCookie name s = maybeLast [ cv | (cn,cv) <- readCookies s, cn == name ]
+
+-- | Set a cookie in HTTP response headers.
+setCookie :: Cookie
+	  -> Headers
+	  -> Headers
+-- The cookie is just added to the end of the headers,
+-- since the last instance of a cookie overrides earlier ones.
+setCookie c hs = hs ++ [("Set-Cookie", showCookie c)]
+
+
+-- | Delete a cookie from the client by setting the cookie expiry date 
+--   to a date in the past.
+deleteCookie :: Cookie  -- ^ Cookie to delete. The only fields that matter
+		        --   are 'cookieName', 'cookieDomain' and 'cookiePath'
+	     -> Cookie
+deleteCookie c = c { cookieExpires = Just epoch }
+    where
+    epoch = CalendarTime {
+			  ctYear = 1970,
+			  ctMonth = January,
+			  ctDay = 1,
+			  ctHour = 0,
+			  ctMin = 0,
+			  ctSec = 0,
+			  ctPicosec = 0,
+			  ctWDay = Thursday,
+			  ctYDay = 1,
+			  ctTZName = "GMT",
+			  ctTZ = 0,
+			  ctIsDST = False
+			 }
+
+--
+-- * Reading and showing cookies
+--
+
+-- | Show a cookie on the format used as the value of the Set-Cookie header.
+showCookie :: Cookie -> String
+showCookie c = concat $ intersperse "; " $
+	        showPair (cookieName c) (cookieValue c) 
+	         : catMaybes [expires, domain, path, secure]
+    where expires = fmap (showPair "expires" . dateFmt) (cookieExpires c)
+	  domain = fmap (showPair "domain") (cookieDomain c)
+	  path = fmap (showPair "path") (cookiePath c)
+	  secure = if cookieSecure c then Just "secure" else Nothing
+	  dateFmt = formatCalendarTime defaultTimeLocale rfc822DateFormat
+
+-- | Show a name-value pair. URI-escapes the name and value.
+showPair :: String -- ^ name
+	 -> String -- ^ value
+	 -> String
+showPair name value = escape name ++ "=" ++ escape value
+    where escape s = escapeString s (not . reserved)
+	  
+
+-- | Gets all the cookies from a Cookie: header value
+readCookies :: String             -- ^ String to parse
+	    -> [(String,String)]  -- ^ Cookie name - cookie value pairs
+readCookies s = case parse parsePairs "" s of 
+					   Left _ -> []
+					   Right ps -> ps
+
+-- | Parse a semicolon-separated sequence of URI-escaped name=value pairs.
+parsePairs :: Parser [(String,String)]
+parsePairs = sepBy parsePair (char ';' >> spaces)
+
+-- | Parse a URI-escaped name=value pair
+parsePair :: Parser (String,String)
+parsePair = do
+	    spaces
+	    name <- many (satisfy isAllowedChar)
+	    spaces
+	    char '='
+	    spaces
+	    value <- many (satisfy isAllowedChar)
+	    return (unEscapeString name, unEscapeString value)
+
+-- | Returns true if the character is allowed unescaped in 
+--   (the HTTP representation of) cookie names and values.
+isAllowedChar :: Char -> Bool
+isAllowedChar c | isSpace c = False
+isAllowedChar ';' = False
+isAllowedChar '=' = False
+isAllowedChar ',' = False
+isAllowedChar _ = True
+
+
+--
+-- Utilities
+--
+
+-- | Compare two strings from equality, ignoring case.
+equalsIgnoreCase :: String -> String -> Bool
+equalsIgnoreCase x y = map toLower x == map toLower y
+
+-- | Return 'Nothing' is the list is empty, otherwise return
+--   the last element of the list.
+maybeLast :: [a] -> Maybe a
+maybeLast [] = Nothing
+maybeLast xs = Just (last xs)
