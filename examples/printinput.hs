c15d92b71052f2dadaa62e3e68bef30a44f7822f
Björn Bringert <bjorn@bringert.net>
2005-06-14 20:14:59 +0000

    Added a simple example.

---
+++ examples/printinput.hs
@@ -0,0 +1,21 @@
+#!/usr/bin/env runghc
+
+{-# OPTIONS_GHC -package cgi #-}
+
+-- | Prints the values of all CGI variables and inputs.
+module Main where
+
+import Network.NewCGI
+
+printinput :: CGI CGIResult
+printinput = 
+    do
+    setHeader "Content-type" "text/plain"
+    vs <- getVars
+    is <- getInputs
+    output ("Environment:\n" ++ prVars vs
+	    ++ "\nInputs:\n" ++ prVars is)
+
+prVars vs = unlines [k ++ ": " ++ x | (k,x) <- vs ]
+
+main = runCGI printinput
