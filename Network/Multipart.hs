d1dea6fe17f61ee45bf4175f39d06ed0559d1187
Björn Bringert <bjorn@bringert.net>
2005-07-20 16:39:31 +0000

    Added type signatures and cleaned up whitespace in Maultipart.

--- Network/Multipart.hs
+++ Network/Multipart.hs
@@ -1,4 +1,4 @@
--- parsing of the multipart format from RFC2045
+-- parsing of the multipart format from RFC2046
 
 -- based on code from WASHMail, © 2001, 2002 Peter Thiemann
 module Network.Multipart where
@@ -13,32 +13,44 @@ import Text.ParserCombinators.Parsec
 -- * Parsing utilities
 --
 
--- RFC 822 LWSP-char
+-- | RFC 822 LWSP-char
+ws1 :: Parser Char
 ws1 = oneOf " \t"
 
+lexeme :: Parser a -> Parser a
 lexeme p = do x <- p; many ws1; return x
 
--- RFC 822 CRLF (but more permissive)
+-- | RFC 822 CRLF (but more permissive)
+crLf :: Parser String
 crLf = try (string "\n\r" <|> string "\r\n") <|> string "\n" <|> string "\r"
 
-lineChar = noneOf "\n\r"
+-- | One line
+lineString :: Parser String
+lineString = many (noneOf "\n\r")
 
-lineString = many lineChar
-
+literalString :: Parser String
 literalString = do char '\"'
 		   str <- many (noneOf "\"\\" <|> quoted_pair)
 		   char '\"'
 		   return str
 
+headerNameChar :: Parser Char
 headerNameChar = noneOf "\n\r:"
 
+especials, tokenchar :: [Char]
 especials = "()<>@,;:\\\"/[]?.="
 tokenchar = "!\"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~" \\ especials
+
+p_token :: Parser String
 p_token = many1 (oneOf tokenchar)
 
+text_chars :: [Char]
 text_chars = map chr ([1..9] ++ [11,12] ++ [14..127])
+
+p_text :: Parser Char
 p_text = oneOf text_chars
 
+quoted_pair :: Parser Char
 quoted_pair = do char '\\'
 		 p_text
 
@@ -58,62 +70,61 @@ data BodyPart = BodyPart [Header] String
 -- body parts end.
 
 p_multipart_body :: String  -- ^ Boundary
-                 -> GenParser Char () MultiPart
+                 -> Parser MultiPart
 p_multipart_body boundary =
     do p_start_boundary boundary
-       bps <- many (p_encapsulation boundary)
+       bps <- many (do
+                    p_transport_padding
+                    crLf
+                    p_body_part boundary)
        string "--"
        p_transport_padding
-       p_epilogue
+       skipMany anyChar -- discard epilogue
        return (MultiPart bps)
 
+p_transport_padding :: Parser String
 p_transport_padding = many ws1
 
-p_encapsulation boundary =
-    do p_transport_padding
-       crLf
-       p_body_part boundary
-
+p_dash_boundary :: String -> Parser ()
 p_dash_boundary boundary =
     do string "--"
        string boundary
        return ()
 
--- preamble and first dash boundary
+-- | Parse preamble and first dash-boundary
+p_start_boundary :: String -> Parser ()
 p_start_boundary boundary = 
     try (p_dash_boundary boundary) 
             <|> (do
-                 lineString
+                 lineString -- discard a preamble line
                  crLf
                  p_start_boundary boundary)
 
-p_epilogue = skipMany anyChar
-
+p_body_part :: String -> Parser BodyPart
 p_body_part boundary = 
     do hs <- p_fields
        crLf
        try (p_dash_boundary boundary >> return (BodyPart hs []))
                <|> (do l <- lineString
-                       cs <- p_body_rest boundary
+                       cs <- p_body_rest
                        return (BodyPart hs (l++cs)))
-
-p_body_rest boundary =
-    do n <- crLf
-       try (p_dash_boundary boundary >> return [])
-               <|> (do l <- lineString
-                       ls <- p_body_rest boundary
-                       return (n++l++ls))
+    where p_body_rest =
+              do n <- crLf
+                 try (p_dash_boundary boundary >> return [])
+                         <|> (do l <- lineString
+                                 ls <- p_body_rest
+                                 return (n++l++ls))
 
 -- 
--- * Header
+-- * Headers
 --
 
 type Header = (String, String)
 
-p_fields :: CharParser () [Header]
+p_fields :: Parser [Header]
 p_fields = many p_field
 
-p_field :: CharParser () Header
+p_field :: Parser Header
 p_field = 
     do name <- many1 headerNameChar
        char ':'
@@ -123,6 +134,7 @@ p_field =
        extraLines <- many extraFieldLine
        return (map toLower name, concat (line:extraLines))
 
+extraFieldLine :: Parser String
 extraFieldLine = 
     do sp <- ws1
        line <- lineString
@@ -133,6 +145,7 @@ extraFieldLine =
 -- * Parameters (for Content-type etc.)
 --
 
+p_parameter :: Parser (String,String)
 p_parameter =
   do lexeme $ char ';'
      p_name <- lexeme $ p_token
@@ -145,12 +158,12 @@ p_parameter =
 --
 
 data ContentType = 
-	ContentType String -- type
-		    String -- subtype
-		    [(String, String)] -- parameters
+	ContentType String             -- ^type
+		    String             -- ^ subtype
+		    [(String, String)] -- ^ parameters
     deriving Show
 
-p_content_type :: CharParser () ContentType
+p_content_type :: Parser ContentType
 p_content_type = 
   do many ws1
      c_type <- p_token
@@ -167,6 +180,7 @@ data ContentTransferEncoding =
 	ContentTransferEncoding String
     deriving Show
 
+p_content_transfer_encoding :: Parser ContentTransferEncoding
 p_content_transfer_encoding =
   do many ws1
      c_cte <- p_token
@@ -180,24 +194,25 @@ data ContentDisposition =
 	ContentDisposition String [(String, String)]
     deriving Show
 
+p_content_disposition :: Parser ContentDisposition
 p_content_disposition =
   do many ws1
      c_cd <- p_token
      c_parameters <- many p_parameter
      return $ ContentDisposition (map toLower c_cd) c_parameters
 
+--
+-- * Using parsers
+--
 
-
-parseMaybe :: CharParser () a -> SourceName -> String -> Maybe a
+parseMaybe :: Parser a -> SourceName -> String -> Maybe a
 parseMaybe p n inp =
   case parse p n inp of
-    Left pError -> Nothing
+    Left _ -> Nothing
     Right x -> Just x
 
-parseSuccessfully :: CharParser () a -> SourceName -> String -> a
+parseSuccessfully :: Parser a -> SourceName -> String -> a
 parseSuccessfully p n inp =
   case parse p n inp of
-    Left pError ->
-      error (show pError)
-    Right x ->
-      x
+    Left pError -> error (show pError)
+    Right x -> x
