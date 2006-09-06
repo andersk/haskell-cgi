752d0de1aae800bfdda7f87b7ed5a0f33ef6a1df
Björn Bringert <bringert@cs.chalmers.se>
2006-09-06 13:36:03 +0000

    Changed old-libs-compat.sh to mk-cgi-compat.sh and updated to build cgi-compat package.

--- old-libs-compat.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-sed -i.orig -e '/Network\.CGI,/ d' -e 's/Build-depends:\(.*\)/Build-depends: \1, fps/' cgi.cabal
-sed -e 's/^module Network\.CGI/module Network.NewCGI/' < Network/CGI.hs > Network/NewCGI.hs
