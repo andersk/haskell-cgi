31ed477d64cd3b3892fd4ac7937c8ba9c8c760c3
Björn Bringert <bjorn@bringert.net>
2008-11-17 09:29:33 +0000

    Allow . in HTTP header tokens. On Mon, Nov 17, 2008 at 6:39 AM, Satoshi Nakamura <snakamura@infoteria.co.jp> wrote: > Hi Bjorn, > > I found a problem when I got a Content-Type using cgi-3001.1.7.0. When I > uploaded Excel file whose Content-Type is "application/vnd.ms-excel", it > returned "application/vnd". > > I've looked into the source and found that it denies period in a subtype > token, but according to RFC 2045 it should be accepted. The attached file is > a patch to fix this problem which uses tspecials in RFC 2045 which doesn't > include period instead of especials, which is appropriate for parsing > Content-Type and Content-Disposition. > > > Best Regards, > > -- > Satoshi Nakamura <snakamura@infoteria.co.jp>

--- Network/CGI/Header.hs
+++ Network/CGI/Header.hs
@@ -293,9 +293,9 @@ buggyLiteralString =
 headerNameChar :: Parser Char
 headerNameChar = noneOf "\n\r:"
 
-especials, tokenchar :: [Char]
-especials = "()<>@,;:\\\"/[]?.="
-tokenchar = "!\"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~" \\ especials
+tspecials, tokenchar :: [Char]
+tspecials = "()<>@,;:\\\"/[]?="
+tokenchar = "!\"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~" \\ tspecials
 
 p_token :: Parser String
 p_token = many1 (oneOf tokenchar)
